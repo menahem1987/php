6d57ac1e5ec089aad2892466b895df3f8989aee9
menahem1987 <51406197+menahem1987@users.noreply.github.com>
2020-04-28 14:52:48 +0300

    Add files via upload

---
+++ blocks/footer.php
@@ -0,0 +1,17 @@
+<footer class="pt-4 my-md-5 pt-md-5 border-top container">
+    <div class="card mb-4 shadow-sm">
+        <div class="col-12 col-md">
+            <div>
+                <img class="mb-2" src="img/her.jpg" alt="" width="24" height="24">
+                <small class="d-block mb-3 text-muted">© 2000-2020</small>
+            </div>
+            <div>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-dark"</a>ИНН:</li>
+                    <li><a class="text-dark">ОГРН:</a></li>
+                    <li><a class="text-dark">Информация на сайте не является публичной офертой</a></li>
+                </ul>
+            </div>
+        </div>
+        </div>
+</footer>
